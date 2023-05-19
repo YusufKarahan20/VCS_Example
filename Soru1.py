4ee804436b6cffdace0a8841647b03bb67b14bac
YusufKarahan20 <ysf.krhn.20@gmail.com>
2023-05-19 04:35:33 +0300

    asal olmayan sayılar
    asal olmayan sayıların tam bölenlerini ekrana yazdırıp asal olanların ortalamasını ekrana yazdırma

--- Soru1.py
+++ Soru1.py
@@ -9,3 +9,9 @@ asal_sayi_toplam = 0
         print(x, "asal sayıdır")           
         asal_sayi += 1
         asal_sayi_toplam += x              #sayı asal ise değerini bu değişkende tutuyorum
+    else:
+        print(x, "sayısı asal değildir tam bölenleri şunlardır=")       #asal sayı değilse ekrana tam bölenlerini yazdırıyorum
+        for i in range(1, x+1):
+            if x % i == 0:
+                print(i)
+print("Asal sayıların ortalaması=", asal_sayi_toplam/asal_sayi)         #en son asal sayıların ortalamasını ekrana yazdırıyorum
