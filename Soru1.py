3cbb85a73b739b50a0d92dccd6853534fb6e892f
YusufKarahan20 <ysf.krhn.20@gmail.com>
2023-05-19 04:28:49 +0300

    asal sayı ayıklama
    for döngüsü ile asal sayıları bulma

--- Soru1.py
+++ Soru1.py
@@ -1,3 +1,7 @@
 sayi = int(input("Kaç tane sayı girmek istersin:"))        
 asal_sayi = 0                                            #kaç tane asal sayı olduğunu belirlemek için değişken atıyorum
 asal_sayi_toplam = 0
+    sayac = 0                            #sayı asal mı değil mi belirlemek için sayaç tutuyorum
+    for i in range(2, x+1):
+        if x % i == 0:
+            sayac += 1
