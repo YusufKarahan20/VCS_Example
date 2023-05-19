3835189f1c0d479f5cacce99ec88a94ffb55241d
YusufKarahan20 <ysf.krhn.20@gmail.com>
2023-05-19 04:17:39 +0300

    for ile döngü
    for yardımı ile döngü başlangıcı

--- Soru1.py
+++ Soru1.py
@@ -1,3 +1,5 @@
 sayi = int(input("Kaç tane sayı girmek istersin:"))        
 asal_sayi = 0                                            #kaç tane asal sayı olduğunu belirlemek için değişken atıyorum
 asal_sayi_toplam = 0
+for i in range(sayi):
+    x = int(input("Sayı giriniz:"))
