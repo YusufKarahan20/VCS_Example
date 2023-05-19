0ef76771dd267c72aa474e5e147869f2522e3ca5
YusufKarahan20 <113550506+YusufKarahan20@users.noreply.github.com>
2023-05-19 04:15:09 +0300

    baslangic_noktasi

---
+++ Soru1.py
@@ -0,0 +1,3 @@
+sayi = int(input("Kaç tane sayı girmek istersin:"))        
+asal_sayi = 0                                            #kaç tane asal sayı olduğunu belirlemek için değişken atıyorum
+asal_sayi_toplam = 0
